42acb5af3a73551285534d1b079046ca97c906b4
Eze-Nat <ezequiel_natale@yahoo.com.ar>
2022-10-18 20:24:21 -0300

    instalado express

--- index.js
+++ index.js
@@ -1,36 +1,63 @@
-class Usuario {
-    constructor(nombre, apellido, libros, mascotas) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.libros = libros;
-        this.mascotas = mascotas;
-    }
+const fs = require("fs");
 
-    getFullName() {
-        console.log(`El usuario es ${this.nombre} ${this.apellido}`)
-    }
+/* 
+const date = Date.now();
+const hoy = new Date(date)
 
-    addMascota(mascota) {
-        this.mascotas.push(mascota);
 
-    }
 
-    countMascotas() {
-    return this.mascotas.length;
-    }
 
-    addBook(nombre, autor) {
-        this.libros.push({nombre, autor})
-    }
+try {
+  fs.writeFileSync("./fyh.txt",`${hoy}`);
 
-    getBookNames() {
-        this.libros.forEach((element) => {
-            console.log(element)
-        });
-    }
+  const fechaEjercicio = fs.readFileSync("./fyh.txt", "utf-8");
+} catch (e) {
+  console.log("invalid");
 }
+ */
 
-const usuario = new Usuario('Ezequiel', 'Natale', [{nombre: 'El señor de las moscas',autor: 'William Golding'}, {nombre: 'Fundacion', autor: 'Isaac Asimov'}], ['Perro', 'Gato']);
+const productos = [
+  { id: 1, nombre: `Escuadra`, precio: 323.45 },
+  { id: 2, nombre: `Calculadora`, precio: 234.56 },
+  { id: 3, nombre: `Globo Terráqueo`, precio: 45.67 },
+  { id: 4, nombre: `Paleta Pintura`, precio: 456.78 },
+  { id: 5, nombre: `Reloj`, precio: 67.89 },
+  { id: 6, nombre: `Agenda`, precio: 78.9 },
+];
+
+/* let nombre = "";
+let mayor = 0;
 
 
-console.log(usuario)
+productos.forEach((element) => {
+nombre = `${nombre}, ${elemente.nombre}`
+total += element.precio;
+
+  if (element.precio > mayor) {
+    mayor = element.precio;
+  }
+
+});
+
+console.log(nombre);
+console.log(total);
+console.log(total/productos.length);
+console.log(mayor); */
+
+const aux = [];
+productos.forEach((e) => {
+  aux.push(e.precio);
+});
+
+const menor = Math.min(...aux);
+const mayor = Math.max(...aux);
+
+const totalProductos = productos.reduce((acc, e) => acc + e.precio, 0);
+const promedio = Math.round(totalProductos / productos.length);
+
+console.log(
+  totalProductos,
+  `El promedio del valor es $${promedio}`,
+  menor,
+  mayor
+);
